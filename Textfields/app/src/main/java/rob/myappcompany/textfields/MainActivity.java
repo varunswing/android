e3af226e105c68bb446f39a27b1276d2e053d6fd
varunswing <varuns939@gmail.com>
2020-05-10 15:40:21 +0530

    Text Fields

---
+++ Textfields/app/src/main/java/rob/myappcompany/textfields/MainActivity.java
@@ -0,0 +1,28 @@
+package rob.myappcompany.textfields;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void login(View view){
+
+        EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+
+        EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        Log.i("Username", usernameEditText.getText().toString());
+
+        Log.i("Password", passwordEditText.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
