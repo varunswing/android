eb6cb882119b91ff8d21f8cbd549f66e32b0dfb2
varunswing <varuns939@gmail.com>
2021-04-11 21:46:29 +0530

    Update MapsActivity.java

--- MapFun/app/src/main/java/rob/myappcompany/mapfun/MapsActivity.java
+++ MapFun/app/src/main/java/rob/myappcompany/mapfun/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,9 +40,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // Add a marker in Everest and move the camera
+        LatLng everest = new LatLng(27.9881, 86.9250);
+        mMap.addMarker(new MarkerOptions().position(everest).title("Biggest In The World").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(everest, 4));
     }
 }
